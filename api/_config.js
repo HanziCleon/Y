b1e5699690bac3cbea9dfad68ed518943ff1f0a9
HanziCleon <137362532+HanziCleon@users.noreply.github.com>
2025-10-16 08:37:27 +0700

    Hardcode MASTER_BIN_ID and MASTER_API_KEY
    Replaced environment variables for MASTER_BIN_ID and MASTER_API_KEY with hardcoded values.

--- api/_config.js
+++ api/_config.js
@@ -2,8 +2,8 @@
 const path = require('path');
 const JSONBin = require(path.join(__dirname, '..', 'lib', 'jsonbin'));
 
-const MASTER_BIN_ID = process.env.MASTER_BIN_ID;
-const MASTER_API_KEY = process.env.MASTER_API_KEY;
+const MASTER_BIN_ID = "68efb31dae596e708f156b67";
+const MASTER_API_KEY = "$2a$10$PsVzgljojE5fq8qZRmpE4uzMr0K9LArqfmumGVSmNY.P8F2iTKrim";
 const ADMIN_EMAIL = process.env.ADMIN_EMAIL || 'hanzgantengno1@gmail.com';
 
 if (!MASTER_BIN_ID || !MASTER_API_KEY) {
