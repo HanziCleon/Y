bebf72474aebb7a4363e5568401ed1eab88d195d
HanziCleon <137362532+HanziCleon@users.noreply.github.com>
2025-11-09 12:25:42 +0700

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  siteName: "Dongtube",
-  authorName: "Hanzy",
-  profilePicture: "https://cdn.nefyu.my.id/csy1.jpg",
-  profileBanner: "https://cdn.nefyu.my.id/cgxl.png",
-  aboutText: "A personal project for file hosting and URL shortening, built with simplicity and efficiency in mind.",
-  backgroundImage: "https://cdn.nefyu.my.id/1lsq.webp"
-};
